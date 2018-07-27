4dfe8194847490738ba19032b1a774bee01b6859
danila <suver.www@mail.ru>
2018-07-27 22:22:46 +0300

    Some improvments have been done

--- src/Golf.hs
+++ src/Golf.hs
@@ -1,42 +1,38 @@
 module Golf where
 
 --exercize 1--
-skip :: [a] -> Int -> Int -> [a]
-skip [] _ _     = []
-skip (x:xs) 0 b = x : skip xs b b 
-skip (_:xs) a b = skip xs (a - 1) b
-
-skipskip :: [a] -> Int -> [[a]]
-skipskip list 0 = [list]
-skipskip list x = skipskip list (x - 1) ++ [skip list x x] 
+skip :: [a] -> Int -> [a]
+skip [] _ = []
+skip xs n = if (n > length xs) 
+            then [] 
+            else let sp = splitAt n xs 
+                 in (last $ fst sp) : skip (snd sp) n
 
 skips :: [a] -> [[a]]
-skips []   = []
-skips list = skipskip list (length list - 1)
+skips []    = []
+skips [x]   = [[x]]
+skips list  = map (skip list) [1..(length list)]
 
 --exercize 2--
 localMaxima :: [Integer] -> [Integer]
-localMaxima (x:y:z:xs) = [y | y > x && y > z] ++ localMaxima (y:z:xs)
+localMaxima (x:xs@(y:z:_)) = [y | y > x && y > z] ++ localMaxima xs
 localMaxima _          = []
 
 --exercize 3--
-count :: [Integer] -> Integer -> Integer -> Integer
-                    -> Integer -> Integer -> Integer
-                     -> Integer -> Integer -> Integer
-                      -> Integer -> [Integer]
-count (x:xs) a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 = case x of
-                                         0 -> count xs (a0 + 1) a1 a2 a3 a4 a5 a6 a7 a8 a9
-                                         1 -> count xs a0 (a1 + 1) a2 a3 a4 a5 a6 a7 a8 a9
-                                         2 -> count xs a0 a1 (a2 + 1) a3 a4 a5 a6 a7 a8 a9
-                                         3 -> count xs a0 a1 a2 (a3 + 1) a4 a5 a6 a7 a8 a9
-                                         4 -> count xs a0 a1 a2 a3 (a4 + 1) a5 a6 a7 a8 a9
-                                         5 -> count xs a0 a1 a2 a3 a4 (a5 + 1) a6 a7 a8 a9
-                                         6 -> count xs a0 a1 a2 a3 a4 a5 (a6 + 1) a7 a8 a9
-                                         7 -> count xs a0 a1 a2 a3 a4 a5 a6 (a7 + 1) a8 a9
-                                         8 -> count xs a0 a1 a2 a3 a4 a5 a6 a7 (a8 + 1) a9
-                                         9 -> count xs a0 a1 a2 a3 a4 a5 a6 a7 a8 (a9 + 1)
-                                         _ -> count xs a0 a1 a2 a3 a4 a5 a6 a7 a8 a9
-count _ a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 = [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9]
+count :: [Integer] -> [Integer] -> [Integer]
+count (x:xs) [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9] = case x of
+                                         0 -> count xs [a0 + 1, a1, a2, a3, a4, a5, a6, a7, a8, a9]
+                                         1 -> count xs [a0, a1 + 1, a2, a3, a4, a5, a6, a7, a8, a9]
+                                         2 -> count xs [a0, a1, a2 + 1, a3, a4, a5, a6, a7, a8, a9]
+                                         3 -> count xs [a0, a1, a2, a3 + 1, a4, a5, a6, a7, a8, a9]
+                                         4 -> count xs [a0, a1, a2, a3, a4 + 1, a5, a6, a7, a8, a9]
+                                         5 -> count xs [a0, a1, a2, a3, a4, a5 + 1, a6, a7, a8, a9]
+                                         6 -> count xs [a0, a1, a2, a3, a4, a5, a6 + 1, a7, a8, a9]
+                                         7 -> count xs [a0, a1, a2, a3, a4, a5, a6, a7 + 1, a8, a9]
+                                         8 -> count xs [a0, a1, a2, a3, a4, a5, a6, a7, a8 + 1, a9]
+                                         9 -> count xs [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9 + 1]
+                                         _ -> count xs [a0, a1, a2, a3, a4, a5, a6, a7, a8, a9]
+count _ list = list
 
 putLn :: [Integer] -> Integer -> String
 putLn [] _ = []
@@ -52,4 +48,4 @@ construct list = if maximum list == 0
                  else putLn list (maximum list) ++ "\n" ++ construct (decMaxByOne list (maximum list))
 
 histogram :: [Integer] -> String
-histogram list = construct (count list 0 0 0 0 0 0 0 0 0 0)
+histogram list = construct (count list (take 10 [0,0..]))
